92682c7837b6062cf67abdd7779172d10410d37f
nishacodes <nishabatra27@gmail.com>
2013-11-12 09:51:39 -0500

    Keep one randomize file

--- todo1/randomize.rb
+++ todo1/randomize.rb
@@ -1,17 +1,3 @@
-names = ['Keith', 'Nisha', 'Jeremy', 'Sara']
 
-def rand_names(array)
-	index = rand(4)
-	puts array[index]
-end
-
-rand_names(names)
-rand_names(names)
-rand_names(names)
-rand_names(names)
-rand_names(names)
-rand_names(names)
-rand_names(names)
-rand_names(names)
-rand_names(names)
-rand_names(names)
+team = ["Sara Gorecki", "Keith Williams", "Jeremy Lewis", "Nisha Batra"]
+puts team[rand(4)]
