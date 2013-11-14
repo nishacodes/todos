d868e33b6893a85528ca99be2d21a4b20e96f8ff
nishacodes <nishabatra27@gmail.com>
2013-11-14 10:08:31 -0500

    fix code to compare birthday and date today

--- todo3/birthday.rb
+++ todo3/birthday.rb
@@ -1,9 +1,13 @@
 class Birthday
 require 'date'
-	def getbirthday
-		puts "When is your birthday?"
-		birthday=gets.chomp
-		puts birthday == Date.today ? "Happy birthday to you!" : "Cool, I'll remember that."
-	end
+
+
+def getbirthday
+puts "When is your birthday? (MM-DD)"
+birthday=gets.chomp
+puts birthday == Date.today.to_s[5,5] ? "Happy birthday to you!" : "Cool, I'll remember that."
+end
 
 end
+
+Birthday.new.getbirthday
