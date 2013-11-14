3f2970eee3676c34c6a2b3609a5a26297e7a3b87
nishacodes <nishabatra27@gmail.com>
2013-11-14 12:43:09 -0500

    create javascript solution

--- Loops/fizzbuzz/fizzbuzz.rb
+++ Loops/fizzbuzz/fizzbuzz.rb
@@ -1,15 +1,13 @@
 101.times do |i|
-next if i==0
-case 
-	when i%3==0 && i%5==0
-		puts "fizzbuzz"
-
-	when i%3==0
-		puts "fizz"
-
-	when i%5==0 
-		puts "buzz"
-	else
-		puts i
+	next if i==0
+	case 
+		when i%3==0 && i%5==0
+			puts "fizzbuzz"
+		when i%3==0
+			puts "fizz"
+		when i%5==0 
+			puts "buzz"
+		else
+			puts i
 	end
 end
