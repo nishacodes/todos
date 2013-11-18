369cde8798c09b0b333218e790a5235a0bd2b983
nishacodes <nishabatra27@gmail.com>
2013-11-18 16:56:20 -0500

    write test for yahtzee; all working

--- tests/yahtzee.rb
+++ tests/yahtzee.rb
@@ -1,7 +1,10 @@
-  def roll_dice
-    Array.new(5) { rand(1..6) }
-  end
+def roll_dice
+  Array.new(5) { rand(1..6) }
+end
 
-  def win? roll
-    roll.uniq.length == 1 ? true : false
-  end
+def win? roll
+  roll.uniq.length == 1 ? true : false
+end
+
+myroll = roll_dice
+puts win?(myroll)
---
+++ tests/yahtzee_spec.rb
@@ -0,0 +1,33 @@
+require "./yahtzee"
+
+	describe "#roll_dice" do
+		
+		it "should have 5 numbers in the array" do
+			expect(roll_dice).to have(5).items  
+		end
+
+		it "should not have a number greater than 6" do
+			expect(roll_dice).not_to include(7)
+		end
+		
+		it "should not have a number less than 1" do
+			expect(roll_dice).not_to include(0)
+		end
+
+		it "should have only numbers between 1 and 6" do
+			expect(roll_dice.join).to match_regex(/[1-6]{5}/)
+		end
+
+	end
+
+	describe "#win?" do
+
+		it "should return true if all the numbers are the same" do
+			expect(win?([5,5,5,5,5])).to be_true
+		end
+
+		it "should return false if all the numbers are not the same" do
+			expect(win?([5,5,5,5,1])).to be_false
+		end
+
+	end
