f31910dae5f3ca617ca3891432f82e1135f08f9e
nishacodes <nishabatra27@gmail.com>
2013-11-18 17:47:53 -0500

    correct descriptions in tests

--- tests/yahtzee_spec.rb
+++ tests/yahtzee_spec.rb
@@ -3,15 +3,20 @@ require "./yahtzee"
 	describe "#roll_dice" do
 		
 		it "should have 5 numbers in the array" do
-			expect(roll_dice).to have(5).items  
+			expect(roll_dice.compact).to have(5).items  
 		end
 
-		it "should not have a number greater than 6" do
-			expect(roll_dice).not_to include(7)
-		end
+		# it "should not have a number greater than 6" do
+		# 	expect(roll_dice).not_to include(7)
+		# end
 		
-		it "should not have a number less than 1" do
-			expect(roll_dice).not_to include(0)
+		# it "should not have a number less than 1" do
+		# 	expect(roll_dice).not_to include(0)
+		# end
+
+		it "should have only numbers between 1 and 6" do
+			expect(roll_dice[1]<7).to be_true
+			expect(roll_dice[3]>6).to be_false
 		end
 
 		it "should have only numbers between 1 and 6" do
