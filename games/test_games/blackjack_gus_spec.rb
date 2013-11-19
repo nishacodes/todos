4766f5a649bf089011e7edf7d46904fe13be70b4
nishacodes <nishabatra27@gmail.com>
2013-11-19 10:18:00 -0500

    write tests for gus' blackjack

--- games/test_games/blackjack_gus_spec.rb
+++ games/test_games/blackjack_gus_spec.rb
@@ -1,15 +1,26 @@
 require "./blackjack_gus"
 
+card
+blackjack
+
 describe "#card" do
-	it "should deal a hand that is less than or equal to 21" do
-		expect(@hand <= 21).to be_true
+	it "should generate a number greater than 0" do
+		expect(card > 0).to be_true
 	end
 	
+	it "should generate a number less than 12" do
+		expect(card < 12).to be_true
+	end
+
 end
 
+
 describe "#blackjack" do
-	it "should deal a hand that is less than or equal to 21" do
-		# test
-	end
 	
+	# having a local variable issue
+	it "should deal two cards between 1 and 11" do
+		expect(card1 < 12 && card1 > 0 ).to be_true
+		expect(card2 < 12 && card2 > 0 ).to be_true
+	end
+
 end
