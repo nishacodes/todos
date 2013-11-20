0aab645f0b15905baf443d4c7d1a8c6f0dd46cc3
nishacodes <nishabatra27@gmail.com>
2013-11-20 17:36:22 -0500

    finish basic structure of pokemon classes

--- pokemon/spritzee.rb
+++ pokemon/spritzee.rb
@@ -1,42 +1,50 @@
 class Spritzee
-  attr_reader :female_ratio
-  attr_accessor :level, :leveling_rate, :type, :abilities, :catch_rate, :entry, :hp, :exp, :friendship
+	attr_reader :female_ratio
+	attr_accessor :level, :leveling_rate, :type, :abilities, :catch_rate, :entry, :hp, :exp
 
-  LEARNSET = {  "Growth" => "Start",
-                "Leech Seed" => "Start",
-                "Mega Drain" => "Start",
-                "Cotton Spore" => "Start",
-                "Gust" => 10,
-                "Tailwind" => 28,
-                "Hurricane" => 46 }
+	LEARNSET = {  "Sweet Scent" => "Start",
+								"Fairy Wind" => "Start",
+								"Sweet Kiss" => "Start",
+								"Odor Sleuth" => "Start",
+								"Echoed Voice" => 10,
+								"Calm Mind" => 28,
+								"Draining Kiss" => 46,
+								"Aromatherapy" => 25,
+								"Attract" => 29,
+								"Moonblast" => 31,
+								"Charm" => 35,
+								"Flail" => 38,
+								"Misty Terrain" => 42,
+								"Skill Swap" => 44,
+								"Psychic" => 48,
+								"Disarming Voice" => 50}
 
-  def initialize
-    # @level = ?
-    @leveling_rate = "medium fast"
-    @type = ["Fairy"]
-    @abilities = ["Healer","Aroma Veil"]
-    @catch_rate = 0.0
-    # @female_ratio = 0.50
-    # @entry = "Whimsicott can pass through the tiniest cracks like the wind, leaving fluffy white puffs in its wake."
-    # @hp = 60
-    # @exp = 168
-    # @friendship = 70
-  end
+	def initialize
+		@level = 0 # ??
+		@leveling_rate = "medium fast"
+		@type = ["Fairy"]
+		@abilities = ["Healer","Aroma Veil"]
+		@catch_rate = 0.0
+		@female_ratio = 0.50
+		@entry = "It emits a scent that enraptures those who smell it. This fragrance changes depending on what it has eaten."
+		@hp = 78
+		@exp = 68
+	end
 
-  # def level_up
-  #   if exp > level**3
-  #     level += 1
-  #   end
-  # end
+	def level_up
+	  if exp > @level**3
+	    @level += 1
+	  end
+	end
 
-  # def gain_exp new_exp
-  #   exp += new_exp
-  # end
+	def gain_exp new_exp
+	  exp += new_exp
+	end
 
-  # def say_name
-  #   "Whimsicott!"
-  # end
+	def say_name
+	  "Spritzee!"
+	end
 
-  
+	
 end
 
