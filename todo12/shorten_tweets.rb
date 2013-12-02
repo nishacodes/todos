b05ed51f9967cbb20cc2cfdce72152cdd677a6a1
nishacodes <nishabatra27@gmail.com>
2013-12-02 16:25:14 -0500

    fix truncate method

--- todo12/shorten_tweets.rb
+++ todo12/shorten_tweets.rb
@@ -2,12 +2,11 @@
 
 
 
-tweets = [
-"Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!",
-"OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?", "I'm running out of example tweets for you guys, which is weird, because I'm a writer and this is just writing and I tweet all day. For real, you guys. For real.",
-"GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!",
-"Four be you and"
-]
+# tweets = [
+# "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!",
+# "OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?", "I'm running out of example tweets for you guys, which is weird, because I'm a writer and this is just writing and I tweet all day. For real, you guys. For real.",
+# "GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!"
+# ]
 
 class ShortenTweet
 	attr_accessor :tweet
@@ -35,21 +34,20 @@ class ShortenTweet
 	def shorten
 		if too_long
 			to_shorten = @tweet.split(/(?=\b)/).collect do |word|
-				SUBSTITUTIONS[word] || word
+				SUBSTITUTIONS[word.downcase] || word
 			end
 			@tweet = to_shorten.join
-			puts @tweet
-			truncatetweet
+			truncatetweet 
 		else
-			puts @tweet
+			@tweet
 		end
 	end
 
 	def truncatetweet
-		@tweet.truncate(140) if too_long
+		@tweet[0..139] if too_long
 		puts @tweet
 	end
 
 end
 
-mytweet = ShortenTweet.new("Four be you and Four be Four be you and Four be Four be you and Four be Four be you and Four be Four be you and Four be Four be you and Four be Four be you and Four be Four be you and Four be you and Four be you and Four be you and Four be you and Four be you and")
+mytweet = ShortenTweet.new("GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!")
