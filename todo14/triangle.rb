3bb6894f43994f7e3fb50f159b8247ee8cca0ac8
nishacodes <nishabatra27@gmail.com>
2013-12-05 10:48:43 -0500

    incorporate raising error when parameters are invalid for a triangle

--- todo14/triangle.rb
+++ todo14/triangle.rb
@@ -5,17 +5,40 @@ class Triangle
     @side1 = side1
     @side2 = side2
     @side3 = side3
+    # @sides << side1
+    # @sides << side2
+    # @sides << side3
     @kind
   end
 
   def kind
-    if side1 == side2 && side1 == side3
+    if checkerror
+      if side1 == side2 && side1 == side3
       :equilateral
-    elsif side1 == side2 || side2 == side3 || side1 == side3
-      :isosceles
-    elsif side1 != side2 && side2 != side3 && side1 != side3
-      :scalene
-      
+      elsif side1 == side2 || side2 == side3 || side1 == side3
+        :isosceles
+      elsif side1 != side2 && side2 != side3 && side1 != side3
+        :scalene     
+      end
+    end
+    
+  end
+
+   def checkerror
+    # check negatives
+    if side1 <= 0 || side2 <= 0 || side3 <= 0
+      raise TriangleError
+    #check inequality
+    elsif (side1 + side2 <= side3) || (side1 + side3 <= side2) || (side2 + side3 <= side1)
+      raise TriangleError, "That's impossible!"
+    else
+      true
     end
   end
+
 end
+
+
+class TriangleError < StandardError
+end
+
