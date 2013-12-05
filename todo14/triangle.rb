19c38598c43d9d93656b509aa891044ef7a5ec71
nishacodes <nishabatra27@gmail.com>
2013-12-05 09:25:14 -0500

    write triangle class and kind method

---
+++ todo14/triangle.rb
@@ -0,0 +1,21 @@
+class Triangle
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1,side2,side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @kind
+  end
+
+  def kind
+    if side1 == side2 && side1 == side3
+      :equilateral
+    elsif side1 == side2 || side2 == side3 || side1 == side3
+      :isosceles
+    elsif side1 != side2 && side2 != side3 && side1 != side3
+      :scalene
+      
+    end
+  end
+end
