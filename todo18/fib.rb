5f75658e48367112cde122dfd2610bdf1cdfff39
nishacodes <nishabatra27@gmail.com>
2013-12-13 13:10:24 -0500

    write solution

--- todo18/fib.rb
+++ todo18/fib.rb
@@ -2,5 +2,9 @@
 # create a method that returns the nth number of the fibonacci sequence
 # fib sequence = 0,1,1,2,3,5,8,13 etc, assume the 1st element is 0 (the first number), the 2nd element is 1, the third element is 1
 def fibo_finder(n)
-  # code goes here
+  fibo = [0,1]
+  (2..n).each do |number|
+    fibo << (fibo[number-2] + fibo[number-1])
+  end
+  fibo[n]
 end
