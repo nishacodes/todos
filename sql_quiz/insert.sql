cc6a79ed6cc89182eb08e152914970918641abfe
nishacodes <nishabatra27@gmail.com>
2013-12-16 17:04:00 -0500

    insert questions

--- sql_quiz/insert.sql
+++ sql_quiz/insert.sql
@@ -6,3 +6,39 @@ value ("armando");
 
 INSERT INTO users (name)
 value ("kate");
+
+INSERT INTO quizzes (name)
+value ("Getting to know us");
+
+INSERT INTO quizzes (name)
+value ("Getting to know us - REMIX");
+
+INSERT INTO questions (content, quiz_id)
+value ("Which of the following is a valid group member's superhero name?", 1);
+
+INSERT INTO questions (content, quiz_id)
+value ("What was Kate for Halloween this year?", 1);
+
+INSERT INTO questions (content, quiz_id)
+value ("What does Armando say when a friend he hasn't heard from in awhile ask him for a favor?", 1);
+
+INSERT INTO questions (content, quiz_id)
+value ("What is Nisha's favorite comeback?", 1);
+
+INSERT INTO questions (content, quiz_id)
+value ("What's Ariel's least favorite vegetable?", 1);
+
+INSERT INTO questions (content, quiz_id)
+value ("Which group member does NOT wash their arms?", 2);
+
+INSERT INTO questions (content, quiz_id)
+value ("Name the ingredients of Armando's Leftovers Jambalaya?", 2);
+
+INSERT INTO questions (content, quiz_id)
+value ("What did Amine say about Nisha?", 2);
+
+INSERT INTO questions (content, quiz_id)
+value ("What's the correct pronunciation of Ariel's name?", 2);
+
+INSERT INTO questions (content, quiz_id)
+value ("Which group memeber was prom king/queen?", 2);
