733634d65627657fac3d3246d8db279bfcd97224
nishacodes <nishabatra27@gmail.com>
2013-12-16 18:53:52 -0500

    take off percent from the grade file

--- sql_quiz/grading.sql
+++ sql_quiz/grading.sql
@@ -1,5 +1,4 @@
-SELECT users.name, sum(NOT choices.correct) as incorrect, sum(choices.correct) as correct, sum(choices.correct*10) as grade
-FROM answers
+SELECT users.name, sum(NOT choices.correct) as incorrect, sum(choices.correct) as correct
 JOIN users on answers.user_id = users.id
 JOIN choices on answers.choice_id = choices.id
 GROUP BY users.id
