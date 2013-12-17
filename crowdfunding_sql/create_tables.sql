1ce8099dec5aaf6028cf00e29a5de7efce7770b1
nishacodes <nishabatra27@gmail.com>
2013-12-17 17:46:27 -0500

    create tables

---
+++ crowdfunding_sql/create_tables.sql
@@ -0,0 +1,20 @@
+CREATE TABLE projects (
+title TEXT, 
+category ENUM('music', 'books', 'charity'),
+funding_goal numeric(15,2),
+start_date DATE,
+end_date DATE
+);  
+
+
+CREATE TABLE users (
+name TEXT, 
+age INTEGER
+);
+
+CREATE TABLE users (
+amount INTEGER,
+project_id INTEGER
+);  
+
+
