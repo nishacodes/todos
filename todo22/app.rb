bcc01729dc86e8e74d8b4e0452131f3e4d1f0a9f
nishacodes <nishabatra27@gmail.com>
2013-12-18 11:20:32 -0500

    add background image gifs for different holidays

--- todo22/app.rb
+++ todo22/app.rb
@@ -4,11 +4,30 @@ Bundler.require
 module Birthday
   class App < Sinatra::Application
   
-    get '/' do
+
+    before do
       @date = Date.today.strftime("%m/%d")
-      @birthday = "10/27"
-      @date == @birthday ? @birthday_today = "YESSS" :  @birthday_today = "NO"
-      erb :index
+      @birthday = "12/18"
+      @christmas = "12/25"
+    end
+    
+    get '/' do # birthday is default home page
+      @image = "confetti" if @birthday == @date
+          @date == @birthday ? @is_it_today = "YESSS" : @is_it_today = "NO"
+      erb :index 
+    end
+
+    get '/:holiday' do
+      case params[:holiday]
+        when "birthday"
+          @image = "confetti" if @birthday == @date
+          @date == @birthday ? @is_it_today = "YESSS" : @is_it_today = "NO"
+        when "christmas"
+          @image = "snow" if @christmas == @date
+          @date == @christmas ? @is_it_today = "YESSS" : @is_it_today = "NO"
+        
+      end
+      erb :index  
     end
 
   end
