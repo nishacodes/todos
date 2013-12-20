7ce11212f17a9fd1f8e0a09b860479a4a2976437
nishacodes <nishabatra27@gmail.com>
2013-12-20 13:09:54 -0500

    write 2 more methods

--- todo24/fakehashwrapper.rb
+++ todo24/fakehashwrapper.rb
@@ -11,11 +11,17 @@ class FakeHashWrapper
 
   def add_new_pair(key, value)  
     hash[key]=value
+  end 
+
+  def a_values
+    a = []
+    hash.each {|k, v| a << v if k[0] == "a"}
+    a
   end
 
-  def find_values_by_key(letter)
+  def keys_reverse
+    hash.keys.sort.reverse
   end
-
  
 end
 
