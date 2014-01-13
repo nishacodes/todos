99d9b3169b355ace6dce12a8d80f9eef53fe93f3
nishacodes <nishabatra27@gmail.com>
2014-01-13 09:20:50 -0500

    create scaffolds for user and tweet

---
+++ todo34/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo34::Application.config.secret_token = 'a4d0a9b05ac79af51db80418cab6c78ea90738706da4ca05e49d5836f8b7978e08c88a35651890707ac78198dab0c8d32f4c7d13a48e5b2551adaf3d0d41e0dc'
