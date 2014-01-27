c1afb55bc0a15bb8e9aa7d7a24494cc8f86e7d7f
nishacodes <nishabatra27@gmail.com>
2014-01-27 09:26:00 -0500

    write spec

---
+++ todo42/spec/firstevenSpec.js
@@ -0,0 +1,5 @@
+describe( "#firstEven", function() {
+  it("returns the first even number in the array", function() {
+    expect(firstEven([1,3,5,6,10,2])).toBe(6);
+  });      
+});
---
+++ todo42/src/first_even.js
@@ -0,0 +1,13 @@
+// prereqs: arrays, methods, conditional logic
+// Write a method that returns the first even element in a array of integers
+// This is an old todo we did in Ruby, please rewrite your answer in javascript
+ 
+function firstEven(items) {
+  for(var i=0; i < items.length; i++) {
+    if (items[i] % 2 === 0) {
+      return items[i];
+      break;
+    }
+  }
+}
+  
